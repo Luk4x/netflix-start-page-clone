a27ed50a5cb2c0e0de65213dfa50a89c77c6bd26
Luk4x <luk4xm4ci3l@gmail.com>
2022-04-21 17:24:58 -0300

    .user-email text updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const answerList = document.querySelectorAll('.answer-paragraph');
 
 faqList.forEach((question, i) => {
     question.addEventListener('click', () => {
-        question.classList.toggle('active');
-        answerList[i].classList.toggle('active');
+        question.classList.toggle('active-q');
+        answerList[i].classList.toggle('active-a');
     });
 });
