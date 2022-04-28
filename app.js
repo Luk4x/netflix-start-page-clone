d543d99962a672a71619d5eb06371e32870d0521
Luk4x <luk4xm4ci3l@gmail.com>
2022-04-28 16:07:10 -0300

    one question at a time sys

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+// question and answer node list
 const faqList = document.querySelectorAll('.question');
 const answerList = document.querySelectorAll('.answer-paragraph');
 
 faqList.forEach((question, i) => {
     question.addEventListener('click', () => {
-        question.classList.toggle('active-q');
-        answerList[i].classList.toggle('active-a');
+        let iOf = 0;
+        let activeI = 0;
+        let permission = true;
+
+        for (const questionOf of faqList) {
+            if (questionOf.classList.contains('active-q')) {
+                questionOf.classList.toggle('active-q');
+                answerList[iOf].classList.toggle('active-a');
+                activeI = iOf;
+                permission = false;
+            }
+            iOf++;
+        }
+
+        if (activeI !== i || permission) {
+            question.classList.toggle('active-q');
+            answerList[i].classList.toggle('active-a');
+        }
     });
 });
